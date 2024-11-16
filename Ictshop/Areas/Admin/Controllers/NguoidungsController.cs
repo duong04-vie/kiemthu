09b658fc3473ad5d255cbd4122a138cb0cd72f4d
duong04-vie <duongche77@gmail.com>
2024-11-16 21:54:07 +0700

    init project

---
+++ Ictshop/Areas/Admin/Controllers/NguoidungsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Areas.Admin.Controllers
+{
+    public class NguoidungsController : Controller
+    {
+        private Qlbanhang db = new Qlbanhang();
+
+        // Xem quản lý tất cả người dùng
+        // GET: Admin/Nguoidungs
+        public ActionResult Index()
+        {
+            var nguoidungs = db.Nguoidungs.Include(n => n.PhanQuyen);
+            return View(nguoidungs.ToList());
+        }
+
+        // Xem chi tiết người dùng theo Mã người dùng
+        // GET: Admin/Nguoidungs/Details/5
+        public ActionResult Details(int? id)
+        {
+            // Kiểm tra cookie userCookie trước khi cho phép xem chi tiết
+            if (!IsUserCookieValid())
+            {
+                return RedirectToAction("Dangnhap", "User");
+            }
+
+            // Nếu không có người dùng có mã được truyền vào thì trả về trang báo lỗi
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Khai báo một người dùng theo mã
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            if (nguoidung == null)
+            {
+                return HttpNotFound();
+            }
+
+            // trả về trang chi tiết người dùng
+            return View(nguoidung);
+        }
+
+        // Chỉnh sửa người dùng
+        // GET: Admin/Nguoidungs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            // Kiểm tra cookie isAdmin và userCookie trước khi cho phép chỉnh sửa
+            if (id == null || !IsAdminCookieValid() || !IsUserCookieValid())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            if (nguoidung == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen", nguoidung.IDQuyen);
+            return View(nguoidung);
+        }
+
+        // POST: Admin/Nguoidungs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNguoiDung,Hoten,Email,Dienthoai,Matkhau,IDQuyen")] Nguoidung nguoidung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguoidung).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDQuyen = new SelectList(db.PhanQuyens, "IDQuyen", "TenQuyen", nguoidung.IDQuyen);
+            return View(nguoidung);
+        }
+
+        // Xoá người dùng 
+        // GET: Admin/Nguoidungs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            // Kiểm tra cookie isAdmin và userCookie trước khi cho phép xóa
+            if (id == null || !IsAdminCookieValid() || !IsUserCookieValid())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            if (nguoidung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nguoidung);
+        }
+
+        // POST: Admin/Nguoidungs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nguoidung nguoidung = db.Nguoidungs.Find(id);
+            db.Nguoidungs.Remove(nguoidung);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Kiểm tra tính hợp lệ của cookie isAdmin
+        private bool IsAdminCookieValid()
+        {
+            HttpCookie adminCookie = Request.Cookies["isAdmin"];
+            return adminCookie != null && adminCookie.Value == "true";
+        }
+
+        // Kiểm tra tính hợp lệ của cookie userCookie
+        private bool IsUserCookieValid()
+        {
+            HttpCookie userCookie = Request.Cookies["userCookie"];
+            return userCookie != null && !string.IsNullOrEmpty(userCookie["UserID"]);
+        }
+
+        // Kiểm tra quyền truy cập khi không có cookie hợp lệ
+        private bool IsUserLoggedIn()
+        {
+            return Request.Cookies["userCookie"] != null;
+        }
+
+        // Đăng xuất
+        public ActionResult Logout()
+        {
+            // Xóa cookie userCookie và isAdmin
+            HttpCookie userCookie = new HttpCookie("userCookie");
+            userCookie.Expires = DateTime.Now.AddDays(-1);  // Đặt ngày hết hạn về quá khứ để xóa cookie
+            Response.Cookies.Add(userCookie);
+
+            HttpCookie adminCookie = new HttpCookie("isAdmin");
+            adminCookie.Expires = DateTime.Now.AddDays(-1);  // Đặt ngày hết hạn về quá khứ để xóa cookie
+            Response.Cookies.Add(adminCookie);
+
+            // Chuyển hướng về trang đăng nhập
+            return RedirectToAction("Dangnhap", "User");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ictshop/Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class UserController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+
+        // ĐĂNG KÝ
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+
+        // ĐĂNG KÝ PHƯƠNG THỨC POST
+        [HttpPost]
+        public ActionResult Dangky(Nguoidung nguoidung)
+        {
+            try
+            {
+                // Thêm người dùng mới
+                db.Nguoidungs.Add(nguoidung);
+                db.SaveChanges();
+
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Dangnhap");
+                }
+
+                return View("Dangky");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection userlog)
+        {
+            string userMail = userlog["userMail"].ToString();
+            string password = userlog["password"].ToString();
+            var islogin = db.Nguoidungs.SingleOrDefault(x => x.Email.Equals(userMail) && x.Matkhau.Equals(password));
+
+            if (islogin != null)
+            {
+                if (userMail == "Admin@gmail.com")
+                {
+                    HttpCookie adminCookie = new HttpCookie("isAdmin", "true");
+                    adminCookie.Expires = DateTime.Now.AddHours(1); // Cookie hết hạn sau 1 giờ
+                    Response.Cookies.Add(adminCookie);
+
+                    Session["use"] = islogin;
+                    return RedirectToAction("Index", "Admin/Home");
+                }
+                else
+                {
+                    // Thêm cookie "isUser" cho tài khoản người dùng thường
+                    HttpCookie userCookie = new HttpCookie("isUser", "true");
+                    userCookie.Expires = DateTime.Now.AddHours(1); // Cookie hết hạn sau 1 giờ
+                    Response.Cookies.Add(userCookie);
+
+                    Session["use"] = islogin;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ViewBag.Fail = "Đăng nhập thất bại";
+                return View("Dangnhap");
+            }
+        }
+
+        public ActionResult DangXuat()
+        {
+            // Xóa session của người dùng khi đăng xuất
+            Session["use"] = null;
+
+            // Xóa cookie "isAdmin" nếu tồn tại
+            if (Request.Cookies["isAdmin"] != null)
+            {
+                HttpCookie adminCookie = new HttpCookie("isAdmin");
+                adminCookie.Expires = DateTime.Now.AddDays(-1); // Đặt ngày hết hạn trong quá khứ
+                Response.Cookies.Add(adminCookie);
+            }
+
+            // Xóa cookie "isUser" nếu tồn tại
+            if (Request.Cookies["isUser"] != null)
+            {
+                HttpCookie userCookie = new HttpCookie("isUser");
+                userCookie.Expires = DateTime.Now.AddDays(-1); // Đặt ngày hết hạn trong quá khứ
+                Response.Cookies.Add(userCookie);
+            }
+
+            // Chuyển hướng người dùng về trang chủ sau khi đăng xuất
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
